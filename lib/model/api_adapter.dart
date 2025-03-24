0dfca248ccaa5b61158a223f4c705805c7087296
jooni <joen7939@gmail.com>
2025-03-24 21:30:30 +0900

    fix typing error

--- lib/model/api_adapter.dart
+++ lib/model/api_adapter.dart
@@ -3,5 +3,5 @@ import 'model_quiz.dart';
 
 List<Quiz> parseQuizs(String responseBody) {
   final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
-  return parsed.map<Quiz>((json) => Quiz.fromJson(json)).toLis();
+  return parsed.map<Quiz>((json) => Quiz.fromJson(json)).toList();
 }
